394346e6b67980ce606a3fb412c8978ade5c9e3f
apkouk <cescorh@prosa.co.uk>
2021-05-09 20:41:42 +0200

    Working with slices

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../store/index";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isLoggedin = useSelector((state) => state.auth.isAuthenticated);
+
+  const logoutButtonHandler = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isLoggedin && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutButtonHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
